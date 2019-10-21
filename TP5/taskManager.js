b6f18b2583b79a3193e3c61c642917aaebf51420
Nelson198 <nteixeira88@gmail.com>
2019-10-21 22:15:31 +0100

    #16 Commit: TP5 --> Atualização da interface do gestor de tarefas (só uma página Web); --> Pequenas alterações no servidor.

--- TP5/taskManager.js
+++ TP5/taskManager.js
@@ -10,13 +10,14 @@ var database = "tasks.json"
 var port = 7777
 
 function handler(res, pathfile, contentType) {
-    res.writeHead(200, { "Content-Type": contentType });
-    fs.readFile(pathfile, function (error, data) {
+    fs.readFile(pathfile, (error, data) => {
         if (!error) {
+            res.writeHead(200, { "Content-Type": contentType });
             res.write(data)
         } else {
             console.log(error)
-            res.write(pug.renderFile("error.pug", { e: error }))
+            res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+            res.write(pug.renderFile("templates/error.pug", { e: error }))
         }
         res.end()
     });
@@ -34,42 +35,30 @@ function recoverInfo(request, callback) {
     }
 }
 
-var server = http.createServer(function (req, res) {
+var server = http.createServer((req, res) => {
     var queryString = url.parse(req.url, true).pathname
 
     console.log(req.method + " " + queryString)
 
     switch (req.method) {
         case "GET":
-            /* Página Web inicial */
+            /* Página Web representativa do gestor de tarefas */
             if ((queryString == "/") || (queryString == "/taskManager")) {
-                res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" })
-                res.write(pug.renderFile("index.pug"))
-                res.end()
-            }
-            /* Página Web com a lista de todas as tarefas submetidas */
-            else if (queryString == "/seeTasks") {
                 jsonfile.readFile(database, (error, tasks) => {
                     res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" })
                     if (!error) {
-                        res.write(pug.renderFile("seeTasks.pug", { list: tasks }))
+                        res.write(pug.renderFile("templates/index.pug", { list: tasks }))
                     } else {
                         var msg = "Erro ao ler a lista de tarefas presente no ficheiro " + database + " !"
                         console.log(msg)
-                        res.write(pug.renderFile("error.pug", { e: msg }))
+                        res.write(pug.renderFile("templates/error.pug", { e: msg }))
                     }
                     res.end()
                 })
             }
-            /* Página Web que permite submeter uma determinada tarefa */
-            else if (queryString == "/submitTask") {
-                res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" })
-                res.write(pug.renderFile("submitTask.pug"))
-                res.end()
-            }
             /* "Stylesheet" */
             else if (queryString == "/w3.css") {
-                handler(res, "w3.css", "text/css")
+                handler(res, "stylesheet/w3.css", "text/css")
             }
             /* Ícone da página Web inicial */
             else if (queryString == "/favicon.ico") {
@@ -81,33 +70,41 @@ var server = http.createServer(function (req, res) {
             }
             /* Imagem GIF presente na página Web relativo a algum erro */
             else if (queryString == "/images/loading.gif") {
-                handler(res, "images/loading.gif", "image/x-icon")
+                handler(res, "images/loading.gif", "image/gif")
             }
             /* Pedido GET desconhecido */
             else {
                 res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" })
-                res.write(pug.renderFile("error.pug", { e: "Pedido GET desconhecido !" }))
+                res.write(pug.renderFile("templates/error.pug", { e: "Pedido GET desconhecido !" }))
                 res.end()
             }
             break
 
         case "POST":
-            /* Página Web que permite submeter uma determinada tarefa */
+            /* Ação correspondente a um envio de uma tarefa */
             if (queryString == "/sendTask") {
-                recoverInfo(req, resultado => {
+                recoverInfo(req, result => {
                     jsonfile.readFile(database, (error, tasks) => {
                         if (!error) {
-                            tasks.push(resultado)
+                            tasks.push(result)
                             jsonfile.writeFile(database, tasks, error => {
                                 if (!error) {
-                                    console.log("Tarefa submetida com sucesso !")
+                                    console.log("Tarefa submetida com sucesso no ficheiro " + database + " !")
                                     res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" })
-                                    res.write(pug.renderFile("submitTask.pug", { msg: "✔️ A sua tarefa foi submetida com sucesso. ️✔️" }))
+                                    res.write(pug.renderFile("templates/index.pug", { list: tasks, msg: "✔️ A sua tarefa foi submetida com sucesso. ️✔️" }))
                                     res.end()
                                 } else {
-                                    console.log(error)
+                                    var msg = "Erro ao escrever no ficheiro " + database + " !"
+                                    console.log(msg)
+                                    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+                                    res.write(pug.renderFile("templates/error.pug", { e: msg }))
                                 }
                             })
+                        } else {
+                            var msg = "Erro ao ler o ficheiro " + database + " !"
+                            console.log(msg)
+                            res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+                            res.write(pug.renderFile("templates/error.pug", { e: msg }))
                         }
                     })
                 })
@@ -115,7 +112,7 @@ var server = http.createServer(function (req, res) {
             /* Pedido POST desconhecido */
             else {
                 res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" })
-                res.write(pug.renderFile("error.pug", { e: "Pedido POST desconhecido !" }))
+                res.write(pug.renderFile("templates/error.pug", { e: "Pedido POST desconhecido !" }))
                 res.end()
             }
             break
@@ -124,7 +121,7 @@ var server = http.createServer(function (req, res) {
         default:
             console.log("Erro! O pedido " + req.method + " não é suportado !")
             res.writeHead(200, "Content-Type: text/html; charset=utf-8")
-            res.write(pug.renderFile("error.pug", { e: "O pedido " + req.method + " não é suportado !" }))
+            res.write(pug.renderFile("templates/error.pug", { e: "O pedido " + req.method + " não é suportado !" }))
             res.end()
             break
     }
